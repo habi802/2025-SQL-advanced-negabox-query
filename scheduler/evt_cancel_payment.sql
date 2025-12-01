e080521c1852a80d3f99720107e95143241873c3
habi802 <shb8002@gmail.com>
2025-12-01 15:33:59 +0900

    [comment] 결제 확인 이벤트 스케줄러 주석 작성

--- scheduler/evt_cancel_payment.sql
+++ scheduler/evt_cancel_payment.sql
@@ -3,6 +3,7 @@ DROP EVENT IF EXISTS evt_cancel_payment;
 CREATE EVENT evt_cancel_payment
 ON SCHEDULE EVERY 1 MINUTE
 DO
+    -- 결제 대기 상태이면서 생성된 지 5분이 지난 데이터의 결제 상태를 '취소' 로 변경
     UPDATE payment
     SET status = 2
     WHERE status = 0

9403919d105ee13694a5f4bb1dd7be0b9abb137e
habi802 <shb8002@gmail.com>
2025-12-01 16:10:41 +0900

    [feat] 결제 after update 트리거 작업 완료

--- trigger/trg_payment_after_update.sql
+++ trigger/trg_payment_after_update.sql
@@ -6,5 +6,19 @@ CREATE TRIGGER trg_payment_after_update
     AFTER UPDATE ON payment
     FOR EACH ROW
 BEGIN
+    -- 결제 상태로 '취소' 나 '환불' 로 변경되었다면
+    if ((OLD.status <> 2 AND NEW.status = 2)
+        OR (OLD.status <> 3 AND NEW.status = 3)) THEN
+        -- 예매의 상태를 '취소' 로 변경
+        UPDATE reservation
+        SET status = 2
+        WHERE reservation_id = OLD.type_id;
 
-end $$
+        -- 스토어 주문 내역의 상태를 '주문 취소' 로 변경
+        UPDATE `order`
+        SET status = 1
+        WHERE order_id = OLD.type_id;
+    END if;
+END$$
+
+DELIMITER ;

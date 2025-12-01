bc88d5e1f6d3e8b769405ba1f03f855603a4efce
habi802 <shb8002@gmail.com>
2025-12-01 16:59:49 +0900

    [feat] 스토어 주문 내역 AFTER UPDATE 트리거 추가

---
+++ trigger/trg_order_after_update.sql
@@ -0,0 +1,18 @@
+DELIMITER $$
+
+DROP TRIGGER IF EXISTS trg_order_after_update;
+
+CREATE TRIGGER trg_order_after_update
+    AFTER UPDATE ON `order`
+    FOR EACH ROW
+BEGIN
+    -- 주문 상태가 '취소' 로 변경되었을 경우
+    if OLD.status <> 1 AND NEW.status = 1 THEN
+        -- 유저별 보유 교환권 상태를 '취소' 로 변경
+        UPDATE user_voucher
+        SET status = 3
+        WHERE order_id = OLD.order_id;
+    END if;
+END$$
+
+DELIMITER ;

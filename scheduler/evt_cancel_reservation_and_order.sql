dc4723a8be94493090590911093e6d74dfed69c8
habi802 <shb8002@gmail.com>
2025-12-02 15:55:18 +0900

    [fix] 예매/스토어 상품 주문 취소 트리거 수정

--- scheduler/evt_cancel_reservation_and_order.sql
+++ scheduler/evt_cancel_reservation_and_order.sql
@@ -1,8 +1,10 @@
+DELIMITER $$
+
 DROP EVENT IF EXISTS evt_cancel_reservation_and_order;
 
 CREATE EVENT evt_cancel_reservation_and_order
 ON SCHEDULE EVERY 1 MINUTE
-DO
+DO BEGIN
     -- 결제 대기 상태이면서 생성된 지 5분이 지난 데이터의 예매, 스토어 주문의 상태를 '취소' 로 변경
     UPDATE reservation
     SET status = 2
@@ -13,3 +15,6 @@ DO
     SET status = 2
     WHERE status = 0
     AND created_at <= NOW() - INTERVAL 5 MINUTE;
+END$$
+
+DELIMITER ;

fc85412aceeea9c3e0cf645fa9020be1fc910c72
habi802 <shb8002@gmail.com>
2025-12-02 15:12:06 +0900

    [refactor] 결제 취소 스케줄러->예매/스토어 상품 주문 취소 스케줄러로 변경

--- trigger/trg_order_after_update.sql
+++ trigger/trg_order_after_update.sql
@@ -7,7 +7,12 @@ CREATE TRIGGER trg_order_after_update
     FOR EACH ROW
 BEGIN
     -- 주문 상태가 '취소' 로 변경되었을 경우
-    if OLD.status <> 1 AND NEW.status = 1 THEN
+    if OLD.status <> 2 AND NEW.status = 2 THEN
+        -- 결제 데이터 상태도 '취소' 로 변경
+        UPDATE payment
+        SET status = 2
+        WHERE type_id = OLD.order_id;
+
         -- 유저별 보유 교환권 상태를 '취소' 로 변경
         UPDATE user_voucher
         SET status = 3
--- trigger/trg_reservation_after_update.sql
+++ trigger/trg_reservation_after_update.sql
@@ -8,6 +8,11 @@ CREATE TRIGGER trg_reservation_after_update
 BEGIN
     -- 예매 상태가 '취소' 로 변경되었을 경우
     if OLD.status <> 2 AND NEW.status = 2 THEN
+        -- 결제 데이터 상태도 '취소' 로 변경
+        UPDATE payment
+        SET status = 2
+        WHERE type_id = OLD.reservation_id;
+
         -- 예매별 예매 좌석 데이터 삭제
         DELETE FROM reservation_seat_list
         WHERE reservation_id = OLD.reservation_id;

3ceac9aa7be621b8d5eadf2a367fcbe88f5523e4
habi <shb8002@gmail.com>
2025-12-04 18:26:15 +0900

    [feat] 결제 AFTER UPDATE 트리거에서 결제 완료 프로시저 호출하는 부분 추가

--- trigger/trg_reservation_after_update.sql
+++ trigger/trg_reservation_after_update.sql
@@ -6,17 +6,34 @@ CREATE TRIGGER trg_reservation_after_update
     AFTER UPDATE ON reservation
     FOR EACH ROW
 BEGIN
+    DECLARE v_payment_method INT;
+    DECLARE v_payment_code VARCHAR(7);
+
     if OLD.status <> 1 AND NEW.status = 1 THEN
         -- 예매 상태가 '완료' 로 변경되었을 경우
         -- 결제 상태를 '완료' 로 변경하는 프로시저 실행
+        SET v_payment_method = FLOOR(RAND() * 3);
+        if v_payment_method = 0 THEN
+            SET v_payment_code = ELT(FLOOR(1 + RAND() * 12),
+                '00501', '00502', '00503', '00504', '00505', '00506',
+                '00507', '00508', '00509', '00510', '00511', '00512'
+            );
+        elseif v_payment_method = 1 THEN
+            SET v_payment_code = ELT(FLOOR(1 + RAND() * 14),
+                '01201', '01202', '01203', '01204', '01205', '01206', '01207',
+                '01208', '01209', '01210', '01211', '01212', '01213', '01214'
+            );
+        elseif v_payment_method = 2 THEN
+            SET v_payment_code = ELT(FLOOR(1 + RAND() * 3),
+                '00901', '00902', '00903'
+            );
+        END if;
+
         CALL update_payment_complete(
              0,
              OLD.reservation_id,
-             0,
-             ELT(FLOOR(1 + RAND() * 12),
-                '00501', '00502', '00503', '00504', '00505', '00506',
-                '00507', '00508', '00509', '00510', '00511', '00512'
-             ),
+             v_payment_method,
+             v_payment_code,
              NULL
         );
     elseif OLD.status <> 2 AND NEW.status = 2 THEN
